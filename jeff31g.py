3de1fac67a9033a2e2f844b7cb08c320cfbc94cf
JEFFERSON <jeffpaulgonzalez2002@gmail.com>
2025-04-01 15:12:36 -0500

    commit inicial

---
+++ jeff31g.py
@@ -0,0 +1,31 @@
+# https://python.org
+
+# espero si poder apremder a programar
+
+"""
+dice q puedo comertar 
+en varias lineas
+y no pasa nada
+# y si quuiero puedo poner un print
+print(¨¨helo¨¨)
+"""
+
+my_variable = "hello wold"
+my_variable = "goodday world"
+
+my_constant = "I am a constant"
+
+my_int = 8
+my_float = 3.1
+my_bool = True
+my_bool = False
+my_string = "cosa q no entendi"
+my_other_string = 'otra cosa q no entendi'
+
+print("!helo,python¡")
+
+print(type(my_int))
+print(type(my_float))
+print(type(my_bool))
+print(type(my_string))
+print(type(my_other_string))
